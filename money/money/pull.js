2fb65912a422d210e12ceff404e48d1f4adfe654
Constants <954615823@qq.com>
2018-04-09 19:08:20 +0800

    k
    k

--- money/money/pull.js
+++ money/money/pull.js
@@ -1,5 +1,8 @@
 $(document).ready(function () {
-    $(".moe2").click(function () {
-        $(".xiah").slideToggle(380);
+    $(".moe2").mouseenter(function () {
+        $(".xiah").slideDown(380);
     });
+    $(".moe2").mouseleave(function () {
+        $(".xiah").slideUp(380);
+    })
 });
