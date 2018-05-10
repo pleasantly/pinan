ae6536990e1341216360efa258c13604cdce7ff5
Constants <954615823@qq.com>
2018-05-10 14:26:49 +0800

    1123
    1123

--- money/money/pull.js
+++ money/money/pull.js
@@ -4,6 +4,13 @@ $(document).ready(function () {
     });
     $(".moe2").mouseleave(function () {
         $(".xiah").slideUp(220);
-    })
+    });
+    // $.ajax({
+    //     url:'http://192.168.18.212:3000/meituan',
+    //     type:'get',
+    //     success:function (e) {
+    //         console.log(e)
+    //     },
+    // })
 });
 
